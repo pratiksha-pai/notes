0b496a73ed0506916ca10c60268d3b6adb8e0adb
gatech <23293632+pratiksha-pai@users.noreply.github.com>
2023-08-21 19:18:39 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Pratiksha's Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
